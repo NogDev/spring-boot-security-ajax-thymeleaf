90ac5ad573796d6d5d203040320079ccf88bdb23
nog <nog.guitar@gmail.com>
2019-08-07 16:31:33 -0300

    Liberando acesso público a home e arquivos estáticos

---
+++ demo-security/src/main/java/com/mballem/curso/security/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.mballem.curso.security.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author andersonnogueira
+ * @since Aug 7, 2019
+ */
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/webjars/**", "/css/**", "/image/**", "/js/**").permitAll()
+			.antMatchers("/", "/home").permitAll()
+			.anyRequest().authenticated();
+	}
+
+}
