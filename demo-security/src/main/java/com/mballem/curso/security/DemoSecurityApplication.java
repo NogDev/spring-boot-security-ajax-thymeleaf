1c0ab880265362580f042c6186d489cd2266006b
nog <nog.guitar@gmail.com>
2019-08-07 17:41:44 -0300

    Configurando aplicação para logar
    - Definido criptografia
    - criando método de consulta de email por usuario para o login

--- demo-security/src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ demo-security/src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoSecurityApplication {
 
 	public static void main(String[] args) {
+		//gerando senha para teste
+		//System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(DemoSecurityApplication.class, args);
 	}
 }
