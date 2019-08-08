7719b39aa6772d56e26a10b5e1e4fb81b8f37b4f
nog <nog.guitar@gmail.com>
2019-08-07 23:29:06 -0300

    Implementando listagem de usuário

--- demo-security/src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
+++ demo-security/src/main/java/com/mballem/curso/security/repository/UsuarioRepository.java
@@ -3,6 +3,8 @@
  */
 package com.mballem.curso.security.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -16,4 +18,10 @@ import com.mballem.curso.security.domain.Usuario;
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 	@Query("select u from Usuario u where u.email like :email")
 	Usuario findByEmail(@Param("email") String email);
+
+	@Query("select distinct u from Usuario u "
+			+ "join u.perfis p "
+			+ "where u.email like %:search% OR p.desc like %:search%" )
+	Page<Usuario> findAllByEmailOrPerfil(String search, Pageable pageable);
+	
 }
--- demo-security/src/main/java/com/mballem/curso/security/service/UsuarioService.java
+++ demo-security/src/main/java/com/mballem/curso/security/service/UsuarioService.java
@@ -4,8 +4,12 @@
 package com.mballem.curso.security.service;
 
 import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -14,6 +18,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.mballem.curso.security.datatables.Datatables;
+import com.mballem.curso.security.datatables.DatatablesColunas;
 import com.mballem.curso.security.domain.Perfil;
 import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.repository.UsuarioRepository;
@@ -28,14 +34,14 @@ public class UsuarioService implements UserDetailsService{
 	@Autowired
 	private UsuarioRepository repository;
 	
+	@Autowired
+	private Datatables datatables;
+	
 	@Transactional
 	public Usuario buscarPorEmail(String email) {
 		return repository.findByEmail(email);
 	}
 
-	/* (non-Javadoc)
-	 * @see org.springframework.security.core.userdetails.UserDetailsService#loadUserByUsername(java.lang.String)
-	 */
 	@Override 	@Transactional
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 		Usuario usuario = buscarPorEmail(username);
@@ -46,16 +52,23 @@ public class UsuarioService implements UserDetailsService{
 				AuthorityUtils.createAuthorityList(getAuthorities(usuario.getPerfis())));
 	}
 
-	/**
-	 * @param perfis
-	 * @return authorities
-	 */
 	private String[] getAuthorities(List<Perfil> perfis) {
 		String[] authorities = new String[perfis.size()];
 		for (int i = 0; i < perfis.size(); i++) {
 			authorities[i] = perfis.get(i).getDesc();
 		}
 		return authorities;
+	}
+
+	
+	@Transactional
+	public Map<String, Object> buscarTodos(HttpServletRequest request) {
+		datatables.setRequest(request);
+		datatables.setColunas(DatatablesColunas.USUARIOS);
+		Page<?> page = datatables.getSearch().isEmpty()
+				? repository.findAll(datatables.getPageable())
+				: repository.findAllByEmailOrPerfil(datatables.getSearch(), datatables.getPageable());
+		return datatables.getResponse(page);
 	}
 	
 }
