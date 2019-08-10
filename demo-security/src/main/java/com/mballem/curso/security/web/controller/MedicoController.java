6eec2edbe6f570c8041a1e63c9d9aed073e6bfc5
nog <nog.guitar@gmail.com>
2019-08-10 10:45:41 -0300

    Dando permissão ao Médico para salvar seus dados cadastrais.

--- demo-security/src/main/java/com/mballem/curso/security/web/controller/MedicoController.java
+++ demo-security/src/main/java/com/mballem/curso/security/web/controller/MedicoController.java
@@ -4,6 +4,8 @@
 package com.mballem.curso.security.web.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,7 +14,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.mballem.curso.security.domain.Medico;
+import com.mballem.curso.security.domain.Usuario;
 import com.mballem.curso.security.service.MedicoService;
+import com.mballem.curso.security.service.UsuarioService;
 
 /**
  * @author andersonnogueira
@@ -26,13 +30,22 @@ public class MedicoController {
 	@Autowired
 	private MedicoService service;
 	
+	@Autowired
+	private UsuarioService usuarioService;
+	
 	@GetMapping({"/dados"})
 	public String abrirPorMedico(Medico medico, ModelMap model) {
 		return "medico/cadastro";
 	}
 
 	@PostMapping({"/salvar"})
-	public String salvar(Medico medico, RedirectAttributes attr) {
+	public String salvar(Medico medico, RedirectAttributes attr, @AuthenticationPrincipal User user) {
+		
+		if(medico.hasNotId() && medico.getUsuario().hasNotId()) {
+			Usuario usuario = usuarioService.buscarPorEmail(user.getUsername());
+			medico.setUsuario(usuario);
+		}
+		
 		service.salvar(medico);
 		attr.addFlashAttribute("sucesso", "Operação realizada com sucesso");
 		attr.addFlashAttribute("medico", medico);
