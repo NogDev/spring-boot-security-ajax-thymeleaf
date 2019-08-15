717e4fbef18e8aef9814ba54d046b1fcadb6287b
nog <nog.guitar@gmail.com>
2019-08-14 21:11:36 -0300

    Salvando e editando Paciente

---
+++ demo-security/src/main/java/com/mballem/curso/security/repository/PacienteRepository.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mballem.curso.security.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.mballem.curso.security.domain.Paciente;
+
+/**
+ * @author andersonnogueira
+ * @since Aug 14, 2019
+ */
+public interface PacienteRepository extends JpaRepository<Paciente, Long>{
+	
+	@Query("select p from Paciente p where p.usuario.email like :email")
+	Optional<Paciente> findByUsuarioEmail(String email);
+
+}
---
+++ demo-security/src/main/java/com/mballem/curso/security/service/PacienteService.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package com.mballem.curso.security.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.mballem.curso.security.domain.Paciente;
+import com.mballem.curso.security.repository.PacienteRepository;
+
+/**
+ * @author andersonnogueira
+ * @since Aug 14, 2019
+ */
+@Service
+public class PacienteService {
+	@Autowired
+	private PacienteRepository repository;
+	
+	public Paciente buscarPorUsuarioEmail(String email) {
+		return repository.findByUsuarioEmail(email).orElse(new Paciente());
+	}
+	
+	@Transactional(readOnly=false)
+	public void salvar(Paciente paciente) {
+		repository.save(paciente);
+	}
+	
+	@Transactional(readOnly=false)
+	public void editar(Paciente paciente) {
+		Paciente p2 = repository.findById(paciente.getId()).get();
+		
+		p2.setNome(paciente.getNome());
+		p2.setDtNascimento(paciente.getDtNascimento());
+	}
+}
