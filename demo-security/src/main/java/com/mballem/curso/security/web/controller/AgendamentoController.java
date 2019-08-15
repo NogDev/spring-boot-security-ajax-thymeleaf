28b7c6e6b9a2298fabbee30d020de5446e5f02a6
nog <nog.guitar@gmail.com>
2019-08-15 13:06:04 -0300

    Disponibilizando formulário de agendamento
    - Liberando acesso do paciente à lista de especialidades
    - Inplementando o autocomplete utilizando JQuery

---
+++ demo-security/src/main/java/com/mballem/curso/security/web/controller/AgendamentoController.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.mballem.curso.security.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mballem.curso.security.domain.Agendamento;
+
+/**
+ * @author andersonnogueira
+ * @since Aug 15, 2019
+ */
+@Controller
+@RequestMapping("agendamentos")
+public class AgendamentoController {
+	
+	@GetMapping({"/agendar"})
+	public String agendarConsulta(Agendamento agendamento) {
+		return "agendamento/cadastro";
+	}
+}
