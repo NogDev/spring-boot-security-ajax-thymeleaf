f810d1c0369a4ef8f0ab9181c291f4a5de91e703
nog <nog.guitar@gmail.com>
2019-08-20 20:08:00 -0300

    Envio de email teste
    - preparando o ambiente para o envio de email
    - realizando o envio teste de email através da classe main

--- demo-security/src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
+++ demo-security/src/main/java/com/mballem/curso/security/DemoSecurityApplication.java
@@ -1,14 +1,32 @@
 package com.mballem.curso.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 
 @SpringBootApplication
-public class DemoSecurityApplication {
+public class DemoSecurityApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		//gerando senha para teste
 		//System.out.println(new BCryptPasswordEncoder().encode("123456"));
 		SpringApplication.run(DemoSecurityApplication.class, args);
+	}
+	
+	@Autowired
+	JavaMailSender sender;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		SimpleMailMessage simple = new SimpleMailMessage();
+		simple.setTo("nog.guitar@gmail.com");
+		simple.setText("Teste número 1");
+		simple.setSubject("teste 1");
+		
+		sender.send(simple);
+		
 	}
 }
